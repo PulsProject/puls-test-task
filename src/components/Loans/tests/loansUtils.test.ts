3e59b6b17ed02100526a844861697bd66a4b2bf4
Vladimir Bondar <vladi.bondar@gmail.com>
2023-09-30 00:53:22 +0200

    add utils tests when no loans received

--- src/components/Loans/tests/loansUtils.test.ts
+++ src/components/Loans/tests/loansUtils.test.ts
@@ -13,6 +13,12 @@ mockLoansByTabsWithEmptyActiveTabList['active loans'] = [];
 const mockTabDetailsWithoutActiveTab = mockTabDetails.filter(
   (tabDetail) => tabDetail.tabValue !== 'active loans',
 );
+const mockLoansByTabsEmpty = { ...mockLoansByTabs };
+mockLoansByTabsEmpty['waiting approval'] = [];
+mockLoansByTabsEmpty['pending requests'] = [];
+mockLoansByTabsEmpty['active loans'] = [];
+mockLoansByTabsEmpty['closed loans'] = [];
+mockLoansByTabsEmpty['rejected loans'] = [];
 
 it('splitLoanRequestsByTabs function should return loan requests split by tabs', () => {
   expect(splitLoanRequestsByTabs(loanRequests)).toEqual(mockLoansByTabs);
@@ -24,6 +30,10 @@ it('splitLoanRequestsByTabs function should return loan requests split by tabs w
   );
 });
 
+it('splitLoanRequestsByTabs function should return loan requests split by tabs with empty arrays', () => {
+  expect(splitLoanRequestsByTabs([])).toEqual(mockLoansByTabsEmpty);
+});
+
 it('mapLoanRequestsToTabDetails function should return filtered out tab details', () => {
   expect(mapLoanRequestsToTabDetails(mockLoansByTabs)).toEqual(mockTabDetails);
 });
@@ -32,4 +42,8 @@ it('mapLoanRequestsToTabDetails function should return filtered out tab details 
   expect(mapLoanRequestsToTabDetails(mockLoansByTabsWithEmptyActiveTabList)).toEqual(
     mockTabDetailsWithoutActiveTab,
   );
+});
+
+it('mapLoanRequestsToTabDetails function should return empty array if no loans received', () => {
+  expect(mapLoanRequestsToTabDetails(mockLoansByTabsEmpty)).toEqual([]);
 });
