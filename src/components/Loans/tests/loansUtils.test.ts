13ed73c346b2374b5d04325142b66a1d9a38dc98
Vladimir Bondar <vladi.bondar@gmail.com>
2023-09-30 00:39:44 +0200

    add tests for loans utils

---
+++ src/components/Loans/tests/loansUtils.test.ts
@@ -0,0 +1,35 @@
+import { mapLoanRequestsToTabDetails, splitLoanRequestsByTabs } from '../loansUtils';
+// eslint-disable-next-line import/extensions
+import data from '../../../data/loans.json';
+import { mockLoansByTabs, mockTabDetails } from './mockData';
+import { LoanRequest } from '../../../data/Interfaces';
+
+const { loanRequests } = data as unknown as { loanRequests: LoanRequest[] };
+const loanRequestsWithoutActiveStatus = loanRequests.filter(
+  (loanRequest) => loanRequest.status !== 'active',
+);
+const mockLoansByTabsWithEmptyActiveTabList = { ...mockLoansByTabs };
+mockLoansByTabsWithEmptyActiveTabList['active loans'] = [];
+const mockTabDetailsWithoutActiveTab = mockTabDetails.filter(
+  (tabDetail) => tabDetail.tabValue !== 'active loans',
+);
+
+it('splitLoanRequestsByTabs function should return loan requests split by tabs', () => {
+  expect(splitLoanRequestsByTabs(loanRequests)).toEqual(mockLoansByTabs);
+});
+
+it('splitLoanRequestsByTabs function should return loan requests split by tabs with empty array for ACTIVE loan tab', () => {
+  expect(splitLoanRequestsByTabs(loanRequestsWithoutActiveStatus)).toEqual(
+    mockLoansByTabsWithEmptyActiveTabList,
+  );
+});
+
+it('mapLoanRequestsToTabDetails function should return filtered out tab details', () => {
+  expect(mapLoanRequestsToTabDetails(mockLoansByTabs)).toEqual(mockTabDetails);
+});
+
+it('mapLoanRequestsToTabDetails function should return filtered out tab details without ACTIVE loan tab', () => {
+  expect(mapLoanRequestsToTabDetails(mockLoansByTabsWithEmptyActiveTabList)).toEqual(
+    mockTabDetailsWithoutActiveTab,
+  );
+});
