7d3f3213b1f400e408bddcffeaf38f5be2c5fc6b
Vladimir Bondar <vladi.bondar@gmail.com>
2023-09-30 23:12:01 +0200

    wp

--- src/components/Loans/TabLabel.tsx
+++ src/components/Loans/TabLabel.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import { TabDetails } from './loansUtils';
 
-const TabLabel: React.FC<TabDetails> = ({ tabValue, tabLabel }) => {
-  const value = tabValue.split('_').join(' ');
-
-  return (
-    <Box sx={{ textTransform: 'capitalize' }}>
-      {value} {tabLabel}
-    </Box>
-  )
-};
+const TabLabel: React.FC<TabDetails> = ({ tabValue, tabLabel }) => (
+  <Box sx={{ textTransform: 'capitalize' }}>
+    {tabValue.split('_').join(' ')} {tabLabel}
+  </Box>
+);
 
 export default TabLabel;
