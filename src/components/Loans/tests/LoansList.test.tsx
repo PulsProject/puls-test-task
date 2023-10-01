a4c3e8e9dc20583355dafac9e92a973a395d7639
Vladimir Bondar <vladi.bondar@gmail.com>
2023-10-02 00:40:56 +0200

    style code

--- src/components/Loans/tests/LoansList.test.tsx
+++ src/components/Loans/tests/LoansList.test.tsx
@@ -6,35 +6,41 @@ import { mockLoansByTabs } from './mockData';
 
 describe('LoansList component', () => {
   it('should display one card when WAITING tab selected', () => {
-    const renderer = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.WAITING]} />);
-    expect(renderer.getAllByTestId('loan-card')).toHaveLength(1);
+    const { getAllByTestId } = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.WAITING]} />);
+
+    expect(getAllByTestId('loan-card')).toHaveLength(1);
   });
 
   it('should display two cards when PENDING tab selected', () => {
-    const renderer = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.PENDING]} />);
-    const loanCards = renderer.getAllByTestId('loan-card');
+    const { getAllByTestId } = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.PENDING]} />);
+    const loanCards = getAllByTestId('loan-card');
+
     expect(loanCards).toHaveLength(2);
     expect(loanCards[0]).toHaveTextContent('R9ZEQCromwell Marketing50.000,00 €52.500,00€ / month');
     expect(loanCards[1]).toHaveTextContent('VZAEMCromwell Marketing19.000,00 €5.358,22€ / month');
   });
 
   it('should display one card when ACTIVE tab selected', () => {
-    const renderer = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.ACTIVE]} />);
-    expect(renderer.getAllByTestId('loan-card')).toHaveLength(1);
+    const { getAllByTestId } = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.ACTIVE]} />);
+
+    expect(getAllByTestId('loan-card')).toHaveLength(1);
   });
 
   it('should display one card when CLOSED tab selected', () => {
-    const renderer = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.CLOSED]} />);
-    expect(renderer.getAllByTestId('loan-card')).toHaveLength(1);
+    const { getAllByTestId } = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.CLOSED]} />);
+
+    expect(getAllByTestId('loan-card')).toHaveLength(1);
   });
 
   it('should display one card when REJECTED tab selected', () => {
-    const renderer = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.REJECTED]} />);
-    expect(renderer.getAllByTestId('loan-card')).toHaveLength(1);
+    const { getAllByTestId } = renderWithTheme(<LoansList loanRequests={mockLoansByTabs[LoanTab.REJECTED]} />);
+
+    expect(getAllByTestId('loan-card')).toHaveLength(1);
   });
 
   it('should have no cards when no loan requests received', () => {
-    const renderer = renderWithTheme(<LoansList loanRequests={[]} />);
-    expect(renderer.queryByTestId('loan-card')).toBeNull();
+    const { queryByTestId } = renderWithTheme(<LoansList loanRequests={[]} />);
+
+    expect(queryByTestId('loan-card')).toBeNull();
   });
 });
